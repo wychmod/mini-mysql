74f79a2999ee24d6779f04c2e0030bdc309aed6c
wychmod <545480453@qq.com>
2024-12-29 13:50:53 +0800

    refactor: 移除未使用的日志记录器
    - 删除了 ConnectionHandlerPerThread 类中未使用的 Logger 静态变量
    - 清理了不必要的导入语句

--- src/main/java/com/wychmod/mysql/ConnectionHandlerPerThread.java
+++ src/main/java/com/wychmod/mysql/ConnectionHandlerPerThread.java
@@ -5,8 +5,6 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @description:
@@ -15,7 +13,6 @@ import org.slf4j.LoggerFactory;
  */
 public class ConnectionHandlerPerThread extends ChannelInboundHandlerAdapter {
 
-    private static final Logger logger = LoggerFactory.getLogger(ConnectionHandlerPerThread.class);
     private final SqlParse sqlParse = new SqlParse();
 
     @Override
